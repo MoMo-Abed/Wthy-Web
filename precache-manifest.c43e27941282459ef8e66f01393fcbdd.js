1ef301cec75c3de7156946682c55694ed9cc234f
MoMo-Abed <benabed678@gmail.com>
2019-04-15 15:18:37 +0200

    Updates

---
+++ precache-manifest.c43e27941282459ef8e66f01393fcbdd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f134a51c3c0a49f33fecc7a201a4e075",
+    "url": "/Wthy-Web/static/media/sunwithcloudwithrain.f134a51c.svg"
+  },
+  {
+    "revision": "fc147247f529f3ed3795",
+    "url": "/Wthy-Web/static/css/main.87bc4b5a.chunk.css"
+  },
+  {
+    "revision": "7741fed744c367518d45de5c3dcb8964",
+    "url": "/Wthy-Web/static/media/background.7741fed7.png"
+  },
+  {
+    "revision": "171ed81019a14f3d21b2",
+    "url": "/Wthy-Web/static/js/1.171ed810.chunk.js"
+  },
+  {
+    "revision": "601436ab8e2a43062978",
+    "url": "/Wthy-Web/static/js/runtime~main.601436ab.js"
+  },
+  {
+    "revision": "109368f632c5dafab6cbf47ec386f8d0",
+    "url": "/Wthy-Web/static/media/cloudywithrain.109368f6.svg"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/Wthy-Web/static/media/cloudy.ad789581.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/Wthy-Web/static/media/storm.7142c78b.svg"
+  },
+  {
+    "revision": "fc147247f529f3ed3795",
+    "url": "/Wthy-Web/static/js/main.fc147247.chunk.js"
+  },
+  {
+    "revision": "f78cba60ad28e30ea060d424f114670a",
+    "url": "/Wthy-Web/static/media/snow.f78cba60.svg"
+  },
+  {
+    "revision": "86b404431c5598a94b7bd930e0b1fe17",
+    "url": "/Wthy-Web/static/media/mist.86b40443.png"
+  },
+  {
+    "revision": "7f93fbdb8a9d02fe414c0a0119b77bd4",
+    "url": "/Wthy-Web/static/media/day.7f93fbdb.svg"
+  },
+  {
+    "revision": "b931d84d75a673fdd8ae1134fefd98ba",
+    "url": "/Wthy-Web/static/media/sunwithcloud.b931d84d.svg"
+  },
+  {
+    "revision": "43524b0a876080ae8d2d7a809537172e",
+    "url": "/Wthy-Web/static/media/cube.43524b0a.svg"
+  },
+  {
+    "revision": "a05a6fd218d55179e611f5ab646adbab",
+    "url": "/Wthy-Web/static/media/deleteicon.a05a6fd2.svg"
+  },
+  {
+    "revision": "171ed81019a14f3d21b2",
+    "url": "/Wthy-Web/static/css/1.ef1284b0.chunk.css"
+  },
+  {
+    "revision": "528db1038690999de65236a23e9eedfd",
+    "url": "/Wthy-Web/index.html"
+  }
+];
